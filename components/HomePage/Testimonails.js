71ddb70bd1195bf23d1d6380f95331ef1f223c78
jalal1985khan <jalal1985khan@gmail.com>
2024-04-24 09:51:49 +0530

    testimonial

--- components/HomePage/Testimonails.js
+++ components/HomePage/Testimonails.js
@@ -7,56 +7,56 @@ const Feedback = [
         'feedback': `“Walmart Vriddhi is a complete growth package for ambitious MSMEs. Its innovative learning and growth approach has been a source of inspiration for us to get through the pandemic.”`,
         'name': 'Umesh Nanda',
         'bussiness': 'Owner, Soumya International, Panipat - Haryana',
-        'desktopImage': 'images/testimonials/desktop-Umesh-Nanda.png',
-        'mobileImage': 'images/testimonials/mobile-Umesh-Nanda.png',
+        'desktopImage': '/images/testimonials/desktop-Umesh-Nanda.png',
+        'mobileImage': '/images/testimonials/mobile-Umesh-Nanda.png',
     },
     {
         'id': '2',
         'feedback': `"Walmart, through the Vriddhi program, has helped me understand what all possible options can be explored during this crisis. It is important for a supplier like me to not stop or give up, even during a pandemic, and Vriddhi has helped me push the pedal here. Even mentorship and advice on financial schemes, and government programs, has helped me navigate through the impact of COVID-19."`,
         'name': 'Babita Gupta',
         'bussiness': 'Founder, Sarangi Creations, Sonipat - Haryana',
-        'desktopImage': 'images/testimonials/desktop-Babita-Gupta.png',
-        'mobileImage': 'images/testimonials/mobile-Babita-Gupta.png',
+        'desktopImage': '/images/testimonials/desktop-Babita-Gupta.png',
+        'mobileImage': '/images/testimonials/mobile-Babita-Gupta.png',
     },
     {
         'id': '3',
         'feedback': `“From business theory to practical measures, from maximizing product quality to minimizing wastage, I’ve quickly applied everything I learned during the first two levels of Walmart Vriddhi. At the same time, I’ve maintained my original business strategy: the end-to-end production of exclusive, never-seen-before designs.”`,
         'name': 'Milan Makwana',
         'bussiness': 'Founder, Maha Rudra Enterprise, Surat - Gujarat',
-        'desktopImage': 'images/testimonials/desktop-Milan-Makwana.png',
-        'mobileImage': 'images/testimonials/mobile-Milan-Makwana.png',
+        'desktopImage': '/images/testimonials/desktop-Milan-Makwana.png',
+        'mobileImage': '/images/testimonials/mobile-Milan-Makwana.png',
     },
     {
         'id': '3',
         'feedback': `“There is a lot to learn when you begin selling digitally, especially with an eye on markets. But with Vriddhi helping us gain the practical know-how of building the business, I am positive that we will be able to tap more markets in the long run.”`,
         'name': 'Mohit Mehta',
         'bussiness': 'Founder, Maha Rudra Enterprise, Surat - Gujarat',
-        'desktopImage': 'images/testimonials/desktop-Mohit-Mehta.png',
-        'mobileImage': 'images/testimonials/mobile-Mohit-Mehta.png',
+        'desktopImage': '/images/testimonials/desktop-Mohit-Mehta.png',
+        'mobileImage': '/images/testimonials/mobile-Mohit-Mehta.png',
     },
     {
         'id': '3',
         'feedback': `“Through Vriddhi, I’ve learnt a lot about different aspects of modern business, including digital sales and developing products for consumer markets. As a result, we are expanding our existing portfolio to include consumer goods and targeting online sales as our main growth channel.”`,
         'name': 'Harpreet Singh',
         'bussiness': 'Owner, Shalimar Rubber & Plastic Industries, Kundli - Haryana',
-        'desktopImage': 'images/testimonials/desktop-Harpreet-Singh.png',
-        'mobileImage': 'images/testimonials/mobile-Harpreet-Singh.png',
+        'desktopImage': '/images/testimonials/desktop-Harpreet-Singh.png',
+        'mobileImage': '/images/testimonials/mobile-Harpreet-Singh.png',
     },
     {
         'id': '3',
         'feedback': `“The clarity of purpose and the guidance that we received from Vriddhi at this crucial time benefited our internal operations as well as sales. It helped us set an example for others. We have aligned our systems with Walmart’s, and we can now focus on innovation.”`,
         'name': 'Sagar Kalra',
         'bussiness': 'Owner, Arch Fab, Panipat - Haryana',
-        'desktopImage': 'images/testimonials/desktop-sagar-kalar.png',
-        'mobileImage': 'images/testimonials/mobile-sagar-kalar.png',
+        'desktopImage': '/images/testimonials/desktop-sagar-kalar.png',
+        'mobileImage': '/images/testimonials/mobile-sagar-kalar.png',
     },
     {
         'id': '3',
         'feedback': `“Walmart’s Vriddhi program has been that guiding light for many MSME owners like me. As a wholesale supplier for the company, I had the good fortune of signing up with the program, which was launched late last year, to train and prepare us to participate in the domestic supply chains.”`,
         'name': 'Shivani Agarwal',
         'bussiness': 'Founder, Scraft Products Pvt. Ltd., Pitampura - Delhi ',
-        'desktopImage': 'images/testimonials/desktop-Shivani-Agarwal.png',
-        'mobileImage': 'images/testimonials/mobile-Shivani-Agarwal.png',
+        'desktopImage': '/images/testimonials/desktop-Shivani-Agarwal.png',
+        'mobileImage': '/images/testimonials/mobile-Shivani-Agarwal.png',
     }
 
 
@@ -82,7 +82,7 @@ function Testimonails() {
 
 
             
-            
+
             <div id="default-carousel" class="relative w-10/12 mx-auto " data-carousel="slide">
                 <div class="relative  overflow-hidden mx-auto">
                 {
