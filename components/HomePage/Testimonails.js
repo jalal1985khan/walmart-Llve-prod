7ccc3d49ada7cb09ae07a51a8396598a55258ae6
jalal1985khan <jalal1985khan@gmail.com>
2024-04-24 12:40:25 +0530

    Test

--- components/HomePage/Testimonails.js
+++ components/HomePage/Testimonails.js
@@ -74,7 +74,7 @@ function Banner() {
         </div>
 
         <div id="default-carousel" class="relative w-full" data-carousel="slide">
-            <div class="relative  overflow-hidden w-4/5 mx-auto">
+            <div class="relative  overflow-hidden  mx-auto">
                 {
                     Banner.map((items, index) => (
                         <div key={index} class="duration-700 ease-in-out h-fit" data-carousel-item >
